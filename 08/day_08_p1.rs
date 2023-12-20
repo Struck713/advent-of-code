1510b0bddb2055bcc080819e79d65f4dc3f995c3
Noah Struck <noahstruck71303@gmail.com>
2023-12-20 02:53:28 -0500

    Added part 1 for 08

---
+++ 08/day_08_p1.rs
@@ -0,0 +1,59 @@
+
+use std::fs;
+use std::vec::Vec;
+use std::time::SystemTime;
+use std::collections::HashMap;
+
+fn main() {
+    let program_start = SystemTime::now();
+    
+    let file_path = "day8.input";
+    let file = fs::read_to_string(file_path).unwrap();
+    
+    let data = file.lines().collect::<Vec<&str>>();
+    let input = data[0];
+
+    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut index = 2;
+    while index < data.len() {
+        let entry = data[index];
+        let entry_key_value = entry.split(" = ").collect::<Vec<&str>>();
+        
+        let key = entry_key_value[0];
+        let left = &entry_key_value[1][1..4];
+        let right = &entry_key_value[1][6..9];
+        
+        map.insert(key, (left, right));
+        index += 1;
+    }
+    
+    let mut current = "AAA";
+    let mut index: usize = 0;
+    let mut score: u64 = 0;
+    loop {
+        if index >= input.len() {
+            index = 0;
+        }
+
+        println!("\"{current}\"");
+        if current == "ZZZ" {
+            break;
+        }
+        
+        let instruction = &input[index..index + 1];
+        let (left, right) = map.get(current).unwrap();
+        if instruction == "L" {
+            current = left;
+        } else {
+            current = right;
+        }
+
+        score += 1;
+        index += 1;
+    }
+
+    println!(":: Score: {score}");
+    if let Ok(elapsed) = program_start.elapsed() {
+        println!(":: Program finished in {}s", elapsed.as_secs());
+    }
+}
