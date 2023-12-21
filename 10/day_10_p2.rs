aa129d77ecb67ea0805a70b8ba9cae298f5dc60c
Struck713 <noahstruck71303@gmail.com>
2023-12-21 01:53:50 -0500

    Worked on 10 part 2

--- 10/day_10_p2.rs
+++ 10/day_10_p2.rs
@@ -12,38 +12,74 @@ fn main() {
                                      .map(|x| x.chars().collect::<Vec<char>>())
                                      .collect::<Vec<Vec<char>>>();
 
+    let mut dots: Vec<(usize, usize)> = Vec::new();
     let mut starting_point: (usize, usize) = (0, 0);
     for i in 0..matrix.len() {
-        if starting_point.0 == 0 && starting_point.1 == 0 {
-            for j in 0..matrix[i].len() {
-                if matrix[i][j] == 'S' {
-                    starting_point = (j, i);
-                    break;
-                }
+        for j in 0..matrix[i].len() {
+            let symbol = matrix[i][j];
+            if symbol == 'S' {
+                starting_point = (j, i);
+            } else if symbol == '.' {
+                dots.push((j, i));
             }
-        } else {
-            break;
         }
     } 
 
-    let mut score = 0;
-    let mut prev_point = (starting_point.0, starting_point.1);
+    // make big polygon (basically)
+    let mut points: Vec<(usize, usize)> = Vec::new();
     let mut point = (starting_point.0, starting_point.1);
+    let mut prev_point = (starting_point.0, starting_point.1);
+
     loop {
-        if point == starting_point && score != 0 {
+        if point == starting_point && points.len() != 0 {
             break;
         }
         
+        points.push(point);
+
         let next_move = find_next_move(&matrix, point, prev_point);
         prev_point = point;
         point = next_move;
-        score += 1;
+
     }
 
-    println!("Score: {}", score / 2); 
+    let mut point_max = (0usize, 0usize);
+    let mut point_min = (usize::MAX, usize::MAX);
+
+    for point in &points {
+        if point > &point_max {
+            point_max = *point;
+        }
+        if point < &point_min {
+            point_min = *point;
+        }
+    }
+
+    dbg!(point_max, point_min);
+    
+    let mut score = 0;
+    for dot in dots {
+        if dot > point_min && dot < point_max && inside_polygon(dot, &points) {
+            score += 1;
+        }
+    }
+
+    for y in 0..11 {
+        for x in 0..11 {
+            if points.iter().any(|other| x == other.0 && y == other.1) {
+                print!("x");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+
+    println!("Score: {}", score); 
 }
 
 // this could be cleaned up but it works!
+// tuple is (x, y, corner)
 fn find_next_move(matrix: &Vec<Vec<char>>, point: (usize, usize), prev_point: (usize, usize)) -> (usize, usize) {
     let (curr_x, curr_y) = point;
     let (prev_x, prev_y) = prev_point;
@@ -57,4 +93,15 @@ fn find_next_move(matrix: &Vec<Vec<char>>, point: (usize, usize), prev_point: (u
         '|' => (curr_x, if prev_y + 1 == curr_y { curr_y + 1 } else { curr_y - 1 }),
         _ => (0, 0)
     }
+}
+
+// really lazy way to check this!
+fn inside_polygon(point: (usize, usize), points: &Vec<(usize, usize)>) -> bool {
+    let mut count_x = 0;
+    for index in 0..point.0 {
+        if points.iter().any(|other| index == other.0 && point.1 == other.1) {
+            count_x += 1;
+        }
+    }
+    count_x % 2 != 0
 }
