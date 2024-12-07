46415a57fc42ebb417bbf9f56a7b8a0fd899c6eb
Struck713 <noahstruck71303@gmail.com>
2024-12-06 20:58:10 -0500

    Completed day 4

--- 2024/challenge/04/day_04.go
+++ 2024/challenge/04/day_04.go
@@ -15,77 +15,76 @@ func (Day04) ID() string {
 func (Day04) Part1(file *os.File) string {
 
 	total := 0
-	matrix := make([][]byte, 0)
+	matrix := make([][]rune, 0)
 	reader := bufio.NewScanner(file)
 	for reader.Scan() {
-		matrix = append(matrix, reader.Bytes())
+		matrix = append(matrix, []rune(reader.Text()))
 	}
 
 	width := len(matrix)
 	height := len(matrix[0])
-	getAndCheckBounds := func(c byte, x int, y int) bool {
-		if x >= 0 && x < width && y >= 0 && y < height {
-			return matrix[y][x] == c
-		}
-		return false
+
+	getAndCheckBounds := func(c rune, x int, y int) bool {
+		return x >= 0 && x < width && y >= 0 && y < height && matrix[x][y] == c
 	}
 
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 
 			// LEFT AND RIGHT
-			if getAndCheckBounds('X', x-3, y) &&
-				getAndCheckBounds('M', x-2, y) &&
-				getAndCheckBounds('A', x-1, y) &&
-				getAndCheckBounds('S', x, y) {
+			if getAndCheckBounds('X', x, y) &&
+				getAndCheckBounds('M', x+1, y) &&
+				getAndCheckBounds('A', x+2, y) &&
+				getAndCheckBounds('S', x+3, y) {
 				total++
 			}
-			if getAndCheckBounds('S', x-3, y) &&
-				getAndCheckBounds('A', x-2, y) &&
-				getAndCheckBounds('M', x-1, y) &&
-				getAndCheckBounds('X', x, y) {
+
+			if getAndCheckBounds('S', x, y) &&
+				getAndCheckBounds('A', x+1, y) &&
+				getAndCheckBounds('M', x+2, y) &&
+				getAndCheckBounds('X', x+3, y) {
 				total++
 			}
 
 			// UP AND DOWN
-			if getAndCheckBounds('X', x, y-3) &&
-				getAndCheckBounds('M', x, y-2) &&
-				getAndCheckBounds('A', x, y-1) &&
-				getAndCheckBounds('S', x, y) {
+			if getAndCheckBounds('X', x, y) &&
+				getAndCheckBounds('M', x, y+1) &&
+				getAndCheckBounds('A', x, y+2) &&
+				getAndCheckBounds('S', x, y+3) {
 				total++
 			}
-			if getAndCheckBounds('S', x, y-3) &&
-				getAndCheckBounds('A', x, y-2) &&
-				getAndCheckBounds('M', x, y-1) &&
-				getAndCheckBounds('X', x, y) {
+			if getAndCheckBounds('S', x, y) &&
+				getAndCheckBounds('A', x, y+1) &&
+				getAndCheckBounds('M', x, y+2) &&
+				getAndCheckBounds('X', x, y+3) {
 				total++
 			}
 
 			// BACKSLASH
-			if getAndCheckBounds('X', x-3, y-3) &&
-				getAndCheckBounds('M', x-2, y-2) &&
-				getAndCheckBounds('A', x-1, y-1) &&
-				getAndCheckBounds('S', x, y) {
+			if getAndCheckBounds('X', x, y) &&
+				getAndCheckBounds('M', x+1, y+1) &&
+				getAndCheckBounds('A', x+2, y+2) &&
+				getAndCheckBounds('S', x+3, y+3) {
 				total++
 			}
-			if getAndCheckBounds('S', x-3, y-3) &&
-				getAndCheckBounds('A', x-2, y-2) &&
-				getAndCheckBounds('M', x-1, y-1) &&
-				getAndCheckBounds('X', x, y) {
+			if getAndCheckBounds('S', x, y) &&
+				getAndCheckBounds('A', x+1, y+1) &&
+				getAndCheckBounds('M', x+2, y+2) &&
+				getAndCheckBounds('X', x+3, y+3) {
 				total++
 			}
 
 			// FORWARD SLASH
-			if getAndCheckBounds('X', x+3, y-3) &&
-				getAndCheckBounds('M', x+2, y-2) &&
-				getAndCheckBounds('A', x+1, y-1) &&
-				getAndCheckBounds('S', x, y) {
+			if getAndCheckBounds('X', x, y) &&
+				getAndCheckBounds('M', x+1, y-1) &&
+				getAndCheckBounds('A', x+2, y-2) &&
+				getAndCheckBounds('S', x+3, y-3) {
 				total++
 			}
-			if getAndCheckBounds('S', x+3, y-3) &&
-				getAndCheckBounds('A', x+2, y-2) &&
-				getAndCheckBounds('M', x+1, y-1) &&
-				getAndCheckBounds('X', x, y) {
+			if getAndCheckBounds('S', x, y) &&
+				getAndCheckBounds('A', x+1, y-1) &&
+				getAndCheckBounds('M', x+2, y-2) &&
+				getAndCheckBounds('X', x+3, y-3) {
 				total++
 			}
 		}
@@ -94,6 +93,40 @@ func (Day04) Part1(file *os.File) string {
 }
 
 func (Day04) Part2(file *os.File) string {
+
 	total := 0
+	matrix := make([][]rune, 0)
+	reader := bufio.NewScanner(file)
+	for reader.Scan() {
+		matrix = append(matrix, []rune(reader.Text()))
+	}
+
+	width := len(matrix)
+	height := len(matrix[0])
+
+	getCharacter := func(x int, y int) rune {
+		if x >= 0 && x < width && y >= 0 && y < height {
+			return matrix[x][y];
+		}
+		return ' '
+	}
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if getCharacter(x, y) == 'A' {
+				pattern := string([]rune{
+					getCharacter(x - 1, y + 1),
+					getCharacter(x + 1, y + 1),
+					getCharacter(x + 1, y - 1),
+					getCharacter(x - 1, y - 1),
+				})
+
+				if pattern == "MSSM" || pattern == "MMSS" || pattern == "SSMM" || pattern == "SMMS" {
+					total++
+				}
+
+			}
+		}
+	}
 	return strconv.Itoa(total)
 }
